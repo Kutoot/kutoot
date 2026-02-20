9bc11904e151bd00ff975ff020679e57a26b62d1
sathishreddy.np <sathishreddy.np@gmail.com>
2026-02-20 14:54:52 +0530

    fix: Use local $faker variable in UserFactory for consistency

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,10 +23,12 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $faker = $this->faker ?? \Faker\Factory::create();
+
         return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
-            'mobile' => $this->faker->unique()->numerify('9#########'),
+            'name' => $faker->name(),
+            'email' => $faker->unique()->safeEmail(),
+            'mobile' => $faker->unique()->numerify('9#########'),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
