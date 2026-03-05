57cf61538685f6fedbc9bb20ea96f5b4041bcfd3
sathishreddy.np <sathishreddy.np@gmail.com>
2026-03-05 07:33:09 +0530

    fix: Update form method type hint from Form to Schema in HeroSettings

--- app/Filament/Pages/HeroSettings.php
+++ app/Filament/Pages/HeroSettings.php
@@ -9,9 +9,9 @@ use Filament\Forms\Components\Toggle;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
-use Filament\Forms\Form;
 use Filament\Notifications\Notification;
 use Filament\Pages\Page;
+use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
 
 class HeroSettings extends Page implements HasForms
@@ -52,7 +52,7 @@ class HeroSettings extends Page implements HasForms
         ]);
     }
 
-    public function form(Form $form): Form
+    public function form(Schema $form): Schema
     {
         return $form
             ->schema([
